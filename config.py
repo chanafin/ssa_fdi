f18d0eac09c5df2a579fdf70f98078de96a65b6f
Chanafin <58449415+chanafin@users.noreply.github.com>
2020-08-11 12:47:04 -0400

    Tuesday Edits

---
+++ config.py
@@ -0,0 +1,13 @@
+# Local Postgres DB Info
+local_db_user = 'postgres'
+local_db_pwd = 'thelog89'
+local_db_endpoint = 'localhost'
+local_db_port = '5432'
+local_db_name = 'FDI_Test'
+
+# Cloud/AWS MySQL DB Info
+remote_db_endpoint = 'ssa-ckh.c7biawpsnxhf.us-east-2.rds.amazonaws.com'
+remote_db_port = '5432'
+remote_db_user = 'postgres'
+remote_db_pwd = 'thelog89'
+remote_db_name = 'ssa-ckh'
